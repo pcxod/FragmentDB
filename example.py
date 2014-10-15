20d31cf01bd1a92bdde97f2be2ac0dcf10c9b350
Daniel Kratzert <dkratzert@gmx.de>
2014-10-15 11:45:19 +0200

    small tests

--- example.py
+++ example.py
@@ -25,22 +25,23 @@ OV.registerFunction(dktest)
 
 
 def match_dbfrag(fragId=17):
-  dbfile = 'C:\Program Files\Olex2-1.2\etc\scripts\dk-database.sqlite'
+  dbfile = 'F:\Programme\Olex2-1.2-dev\etc\scripts\dk-database.sqlite'
   db = FragmentTable(dbfile)
   #print(db.get_all_fragment_names())
   for i in db.get_fragment(fragId)[1]:
-    #print('adding {}'.format(i[0]))
-    print('adding {}  {}  {}  {}  {}'.format(*i))
+    #print('adding {}  {}  {}  {}  {}'.format(*i))
     label = str(i[0])
     x, y, z = float(i[2]), float(i[3]), float(i[4])
     x, y, z = olx.xf.au.Fractionalise(x,y,z).split(',')
     id = olx.xf.au.NewAtom(label, x, y, z)
+    print('adding {}, Id: {}'.format(i[0], id))
     # set occupancy using olx.xf.au.SetAtomOccupancy(id, value)
-    #olx.xf.au.SetAtomOccupancy(id, 1)
+    olx.xf.au.SetAtomOccu(id, 1)
     # or U/Uiso using olx.xf.au.SetAtomU(id, *[U values])
     olx.xf.au.SetAtomU(id, 0.04)
   #olx.Fuse()
   olx.xf.EndUpdate()
+
 
 OV.registerFunction(match_dbfrag)
 
@@ -50,5 +51,5 @@ def find_frag(name):
   for num, name in db.find_fragment_by_name(name):
     print(num, name)
 
-
 OV.registerFunction(find_frag)
+
