950fe1f6d229dbc5ff40886b53837d7013f76602
Daniel Kratzert <dkratzert@gmx.de>
2015-03-23 11:25:07 +0100

    sdf

--- update-olx-plugin.bat
+++ update-olx-plugin.bat
@@ -9,7 +9,7 @@ set GIT="D:\GitHub\DSR-db\plugin-Fragment_DB"
 xcopy /Y %GIT%\Fragment_DB.htm %FDBDIR%
 xcopy /Y %GIT%\Fragment_DB.phil %FDBDIR%
 xcopy /Y %GIT%\Fragment_DB.py %FDBDIR%
-rem xcopy /Y %GIT%\fragment-database.sqlite %FDBDIR%
+xcopy /Y %GIT%\fragment-database.sqlite %FDBDIR%
 xcopy /Y %GIT%\FragmentDB_handler.py %FDBDIR%
 xcopy /Y %GIT%\inputfrag.htm %FDBDIR% 
 
