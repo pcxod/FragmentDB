d1cba288e4bf3eaeac4c4df8ee6923509525b274
Daniel Kratzert <dkratzert@gmx.de>
2015-06-15 20:32:26 +0200

    mac copy script

---
+++ update-olx-plugin.sh
@@ -0,0 +1,18 @@
+#!bash
+
+FDBDIR="/Users/daniel/Downloads/olex2-gui/util/pyUtil/PluginLib"
+GIT="/Users/daniel/GitHub/DSR-db"
+
+
+cp -v  $GIT/Fragment_DB.htm $FDBDIR
+cp -v $GIT/Fragment_DB.phil $FDBDIR
+cp -v $GIT/Fragment_DB.py $FDBDIR
+cp -v $GIT/FragmentDB_handler.py $FDBDIR
+cp -v $GIT/inputfrag.htm $FDBDIR
+
+cp -v $GIT/fragment-database.sqlite $FDBDIR
+
+#rem ##########################################
+
+
+rm $FDBDIR/*.pyc
