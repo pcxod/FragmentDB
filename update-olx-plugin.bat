657300285c98e7f7b6d4f7b5ac64dd5c2563c325
Daniel Kratzert <dkratzert@gmx.de>
2015-10-05 11:10:52 +0200

    removed copy of stable plugin

--- update-olx-plugin.bat
+++ update-olx-plugin.bat
@@ -2,23 +2,23 @@
 
 
 echo %FDBDIR%
-set FDBDIR="C:\Program Files\Olex2-1.2\util\pyUtil\PluginLib\plugin-Fragment_DB"
-set FDBDIR2="D:\downloads\olex2-gui\util\pyUtil\PluginLib\plugin-Fragment_DB"
+rem set FDBDIR="C:\Program Files\Olex2-1.2\util\pyUtil\PluginLib\plugin-Fragment_DB\"
+set FDBDIR2="D:\downloads\olex2-gui\util\pyUtil\PluginLib\plugin-Fragment_DB\"
 set GIT="D:\GitHub\DSR-db"
 
 
-xcopy /Y %GIT%\Fragment_DB.htm %FDBDIR%
-xcopy /Y %GIT%\helptext.htm %FDBDIR%
-xcopy /Y %GIT%\inputfrag.htm %FDBDIR% 
-xcopy /Y %GIT%\Fragment_DB.phil %FDBDIR%
+rem xcopy /Y %GIT%\Fragment_DB.htm %FDBDIR%
+rem xcopy /Y %GIT%\helptext.htm %FDBDIR%
+rem xcopy /Y %GIT%\inputfrag.htm %FDBDIR% 
+rem xcopy /Y %GIT%\Fragment_DB.phil %FDBDIR%
 
-xcopy /Y %GIT%\Fragment_DB.py %FDBDIR%
-xcopy /Y %GIT%\helper_functions.py %FDBDIR% 
-xcopy /Y %GIT%\FragmentDB_handler.py %FDBDIR%
+rem xcopy /Y %GIT%\Fragment_DB.py %FDBDIR%
+rem xcopy /Y %GIT%\helper_functions.py %FDBDIR% 
+rem xcopy /Y %GIT%\FragmentDB_handler.py %FDBDIR%
 
 
-xcopy /Y %GIT%\fragment-database.sqlite %FDBDIR%
-del %FDBDIR%\*.pyc
+rem xcopy /Y %GIT%\fragment-database.sqlite %FDBDIR%
+rem del %FDBDIR%\*.pyc
 
 rem ##########################################
 
@@ -26,16 +26,18 @@ xcopy /Y %GIT%\Fragment_DB.htm %FDBDIR2%
 xcopy /Y %GIT%\helptext.htm %FDBDIR2%
 xcopy /Y %GIT%\inputfrag.htm %FDBDIR2% 
 xcopy /Y %GIT%\Fragment_DB.phil %FDBDIR2%
-
 xcopy /Y %GIT%\Fragment_DB.py %FDBDIR2%
 xcopy /Y %GIT%\helper_functions.py %FDBDIR2% 
 xcopy /Y %GIT%\FragmentDB_handler.py %FDBDIR2%
+
+xcopy /Y %GIT%\fragment-database.sqlite %FDBDIR2%
+
 
 xcopy /Y %FDBDIR2%\FragmentDB_handler.pyc %GIT%
 xcopy /Y %FDBDIR2%\Fragment_DB.pyc %GIT%
 xcopy /Y %FDBDIR2%\helper_functions.pyc %GIT%
 
-xcopy /Y %GIT%\fragment-database.sqlite %FDBDIR2%
+
 
 
 
