4f071adca2fe7790091d81d18a90f2bca1964ce1
Daniel Kratzert <dkratzert@gmx.de>
2015-10-27 18:50:19 +0100

    playing with residuals

--- refine_model_tasks.py
+++ refine_model_tasks.py
@@ -3,12 +3,9 @@ Created on 23.10.2015
 
 @author: daniel
 '''
-from __future__ import print_function
-import olex_core
-from olexFunctions import OlexFunctions
-import olx
+import pprint
 
-OV = OlexFunctions()
+
 
 class Refmod(object):
     '''
@@ -18,16 +15,43 @@ class Refmod(object):
     def __init__(self):
       '''
       Constructor
+      
       '''
+      lstfile = 'd:\Programme\DSR\example\p21c.lst'
+      final = False
+      disag = False
+      disargeelist = []
+      with open(lstfile, 'r') as f:
+        for num, line in enumerate(f):
+          if line.startswith(" Final Structure Factor"):
+            final = True
+            strucfactline = num
+          if line.startswith(' Total number of'):
+            parameters = line.split()[6]
+          if final and line.startswith(' wR2 ='):
+            data = line.split()[7]
+          if final and line.startswith(' Disagreeable restraints'):
+            disag = True
+          if disag: 
+            disargeelist.append(line.split())
+          if line.startswith(' Summary of restraints'):
+            final = False
+            disag = False         
+      del disargeelist[:4]
+      del disargeelist[-3:]
+      print('residuals:', strucfactline, 'data:', data, 'parameters:', parameters)
+      pprint.pprint(disargeelist)
     
     def open_lst_file(self):
       '''
       opens the resulting shelx lst file
       '''
-      ed = olx.GetVar('defeditor')
-      print(ed)
+      pass
+      #ed = olx.GetVar('defeditor')
+      #print(ed)
     
     def get_bond_list(self):
       pass
     
-    
+if __name__ == '__main__':
+  ref = Refmod()
