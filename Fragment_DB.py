c0b64487d71460e38a018bd7e33e687c2ac60e5d
Daniel Kratzert <dkratzert@gmx.de>
2015-11-05 17:07:11 +0100

    helptext and comment

--- Fragment_DB.py
+++ Fragment_DB.py
@@ -29,7 +29,7 @@ Fragen und Ideen:
 - How do I get the Id of a selected atom?
 - onreturn="html.Call(~name~.onchange)"
 - How to make a link without underline?
-
+- Write sqlite to DSR text database export method.
 '''
 
 instance_path = OV.DataDir()
